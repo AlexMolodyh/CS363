bac2a32998a42da82b2785a47728638249ea19fc
= <amolodyh16@mail.wou.edu>
2017-10-18 21:26:12 -0700

    renamed folders to not have spaces

---
+++ Week3/Lab1/homework/guesser.h
@@ -0,0 +1,12 @@
+//
+// Created by Almania on 10/16/2017.
+//
+
+#ifndef CLION_GUESSER_H
+
+int equalsStr(char *pStrInput, char *pStrToMatch);
+void guess();
+
+#define CLION_GUESSER_H
+
+#endif //CLION_GUESSER_H
