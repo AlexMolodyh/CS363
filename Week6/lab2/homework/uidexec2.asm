e42bad8578efb232a35bbdc66d148f8e4a70bc62
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-10-28 18:51:15 -0400

    now get null argv[0]

--- Week6/lab2/homework/uidexec2.asm
+++ Week6/lab2/homework/uidexec2.asm
@@ -5,10 +5,7 @@ _start:
     xor ecx, ecx
     mul ecx
     push ecx
-    push 0x0a4449
-    push 0x5524206f
-    push 0x6863655c
-    push 0x6e69622f
+    push 0x6f686365
     mov ebx, esp
     mov al, 11
     int 0x80
