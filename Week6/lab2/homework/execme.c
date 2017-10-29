eb6fd7a6aa16ed6dab48d520b009c925796c5b9a
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-10-29 15:43:26 -0400

    sinding

--- Week6/lab2/homework/execme.c
+++ Week6/lab2/homework/execme.c
@@ -11,5 +11,17 @@
 
 void main()
 {
-	execve("/usr/bin/id", " -u", NULL);
+	char filename[] = "/bin/id\x00";
+	char uid[] = "-u\x00";
+	char **envp = malloc(sizeof(char) * 4);
+
+	char **argv = malloc(sizeof(char) * 25);
+	argv[0] = malloc(sizeof(char) * 14);
+	argv[1] = malloc(sizeof(char) * 9);
+
+	argv[0] = filename;
+	argv[1] = uid;
+	envp[0] = 0;
+
+	execve(filename, argv, envp);
 }
