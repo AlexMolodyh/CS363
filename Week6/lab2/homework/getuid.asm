aa5f0462ef3993a800e364cf9ccfcd0475c07e9a
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-10-29 23:08:49 -0400

    working on shellcode

--- Week6/lab2/homework/getuid.asm
+++ Week6/lab2/homework/getuid.asm
@@ -1,15 +1,17 @@
-push   0x64692f         ; Push /id
-push   0x6e69622f       ; Push /bin
-push   0x7273752f       ; push /usr
+BITS 32
+
+push   dword 0x64692f         ; Push /id
+push   dword 0x6e69622f       ; Push /bin
+push   dword 0x7273752f       ; push /usr
 mov    eax, esp         ; Store ptr to /usr/bin/id in eax
 
-push   0x752d	        ; Push -u
+push   dword 0x752d	        ; Push -u
 mov    ebx, esp         ; Store ptr to -u in ebx
 
 push   0x752d           ; Push -u
 mov    ecx, esp         ; Store ptr to -u
 
-push   0x00             ; NULL args terminator
+push   0            ; NULL args terminator
 push   ecx              ; Push -u ptr
 push   ebx              ; Push -u ptr
 push   eax              ; Push /usr/bin/id ptr
