bf25c98ab1ad0fca01c71ab8133fc22d8a63c2e8
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-11-14 22:01:20 -0800

    added week 8 content

---
+++ Week6/lab2/homework/working/printUid.asm
@@ -0,0 +1,20 @@
+BITS 32
+
+[SECTION .text]
+
+global _start
+
+_start:
+  xor eax, eax      
+  push eax          
+  push 0x64692f2f   ; push "//id"
+  push 0x6e69622f   ; push "/bin" 
+  push 0x7273752f
+  mov ebx, esp      ; put the address of "/usr/bin//id" into ebx, via esp
+  push eax        
+  mov edx, esp      
+  push ebx          ; push string addr to stack 
+  mov ecx, esp      ; this is the argv array 
+  mov al, 11        ; syscall 11
+  int 0x80         
+
